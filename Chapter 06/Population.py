4133db49718be618627b7e2213223092cea8d05c
myak555 <33710266+myak555@users.noreply.github.com>
2017-11-16 15:00:54 +1000

    Add files via upload

---
+++ Chapter 06/Population.py
@@ -0,0 +1,59 @@
+from Utilities import *
+
+#
+# Describes population (as a Malthus-Velhurst equation)
+# P0 - starting population
+# Q0 - starting renewable resource
+# O0 - renewable resource recovered per year
+# T2 - time for population to double
+# d_rate - natural renewable resource depreciation
+#
+class Population:
+    def __init__( self, P0, Q0, O0, T2, d_rate):
+        self.P_Initial = P0
+        self.P = P0
+        self.Q_Initial = Q0
+        self.Q = Q0
+        self.O_Initial = O0
+        self.O = O0
+        self.B = np.log(2)/T2
+        self.D = d_rate
+        self.Calibration_Year, self.Calibration_Total = Load_Calibration( "Population_calibration.csv", "Year", "Population")
+        self.UN_Low = Bathtub( 1997, 0.0380, 2100, 0.0250, 1390, 12000, 1800)
+        self.UN_Low.Name = "UN Low Case"
+        self.UN_Medium = Sigmoid( 2003, 0.0350, 1390, 11400)
+        self.UN_Medium.Name = "UN Medium Case"
+        self.UN_High = Sigmoid( 2043, 0.0240, 1000, 20000)
+        self.UN_High.Name = "UN High Case"
+        return
+    def dP_dt( self, t):
+        tmp = max ( self.Q, 0.01)
+        tmp = self.B * (1 - self.P / tmp)
+        tmp *= self.P
+        return tmp
+    def dQ_dt( self, t):
+        tmp = self.O
+        tmp -= self.D * self.Q + self.P
+        return tmp
+    def dO_dt( self, t):
+        tmp = 0
+        return tmp
+    def _func( self, y, t):
+        self.P = max( [y[0], 0])
+        self.Q = max( [y[1], 0])
+        self.O = max( [y[2], 0])
+        f0 = self.dP_dt( t)
+        f1 = self.dQ_dt( t)
+        f2 = self.dO_dt( t)
+        return [f0, f1, f2]
+    def Solve( self, t0):
+        y0 = [self.P, self.Q, self.O]
+        soln = odeint(self._func, y0, t0, h0=0.01, hmax = 0.025)
+        self.Solution_Year = t0
+        self.Solution_Total = soln[:, 0].clip(0)
+        self.Solution_Q = soln[:, 1].clip(0)
+        self.Solution_O = soln[:, 2].clip(0)
+        self.P = self.P_Initial
+        self.Q = self.Q_Initial
+        self.O = self.O_Initial
+        return
---
+++ Chapter 06/Test_1_Calhoun.py
@@ -0,0 +1,28 @@
+from Population import *
+
+T_adult =        [1, 104, 315, 560, 736,1444,1471,1588]
+P_adult =        [8,   8, 620,2200,2056, 122, 100,  27]
+P_adult_error =  [0,   0,  30, 100, 100,   0,   0,   0]
+T_litter =       [1, 104, 300, 600, 736,1444,1471,1588]
+P_litter =       [0,  12, 800,   8,   0,   0,   0,   0]
+P_litter_error = [0,   2, 200,   8,   0,   0,   0,   0]
+
+Prepare_Russian_Font()
+fig = plt.figure( figsize=(15,10))
+#ax = plt.subplot()
+#ax.set_yscale("log", nonposx='clip')
+plt.errorbar( T_adult, P_adult, fmt='o', yerr=P_adult_error, color="r", label="Популяция взрослых")
+plt.errorbar( T_litter, P_litter, fmt='o', yerr=P_litter_error, color="g", label="Популяция мышат")
+plt.plot( T_adult, P_adult, "--", lw=1, color="r", label="Взрослые мыши (линейная интерполяция)")
+plt.plot( T_litter, P_litter, "--", lw=1, color="g", label="Мышата (линейная интерполяция)")
+#plt.plot( P.Solution_Time, P.Solution_L, "-", lw=1, color="b", label="Приплод")
+#plt.plot( P.Solution_Time, P.Solution_P, "-", lw=1, color="r", label="Общее количество мышей")
+plt.xlabel("Дни")
+plt.xlim( 0, 1600)
+plt.ylabel("Число мышей")
+plt.ylim( -50, 2950)
+plt.title( 'Популяция Вселенной-25 (по статье Кэлхуна)')
+plt.grid(True)
+plt.legend(loc=0)
+plt.savefig( ".\\Graphs\\figure_06_01.png")
+fig.show()
