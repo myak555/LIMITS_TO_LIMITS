32a31ef1e346783a078ccd905fa68e0c9a361fc9
myak555 <33710266+myak555@users.noreply.github.com>
2017-11-16 15:00:12 +1000

    Add files via upload

---
+++ Chapter 05/Test_4_Kapitsa.py
@@ -0,0 +1,117 @@
+from Population import *
+
+#
+# Описывает популяцию по методу С.П.Капицы
+# "Успехи физических наук" 139(1) 57-71, РАН, 1996
+#
+class Population_Kapitsa:
+    def __init__( self, T0=2007, K=186e3, tau=42):
+        self.T0 = T0
+        self.K = K
+        self.tau = tau
+        return
+    def Solve( self, t):
+        self.Solution_Time = t
+        self.Solution_P = self.K / self.tau * (np.pi/2 - np.arctan((self.T0 - t)/self.tau))
+        return self.Solution_P
+
+#
+# Описывает популяцию по формуле фон-Fёрстера
+# von Foerster, Mora, and Amiot 1960
+# Doomsday: Friday, 13 November, A.D. 2026
+#
+class Population_vonFoerster:
+    def __init__( self, T0=2026.87, K=1.79e5, k=0.990, P_max=1000000):
+        self.T0 = T0
+        self.K = K
+        self.k = k
+        self.P_max = P_max
+        return
+    def Solve( self, t):
+        self.Solution_Time = t
+        self.Solution_P = np.ones( len(t)) * self.P_max
+        for i in range( len(t)):
+            if t[i]>=self.T0: continue
+            self.Solution_P[i] = self.K / (self.T0 - t[i])**self.k
+        return
+
+#
+# Описывает популяцию по формуле фон-Хёрнера
+# Sebastien von Hoerner
+# "Population Explosion and Interstellar Expansion".
+# Journal of the British Interplanetary Society (28): 691–712.
+#
+class Population_vonHoerner:
+    def __init__( self, T0=2026.87, K=2e5, P_max=1000000):
+        self.T0 = T0
+        self.K = K
+        self.P_max = P_max
+        return
+    def Solve( self, t):
+        self.Solution_Time = t
+        self.Solution_P = np.ones( len(t)) * self.P_max
+        for i in range( len(t)):
+            if t[i]>=self.T0: continue
+            self.Solution_P[i] = self.K / (self.T0 - t[i])
+        return
+
+#
+# Описывает популяцию по полному решению уравнения фон-Fёрстера
+#
+class Population_vonFoerster_Full:
+    def __init__( self, T0=2026.87, a=0.0001, K=21, P_max=1000000):
+        self.T0 = T0
+        self.A = a
+        self.K = K
+        self.P_max = P_max
+        return
+    def Solve( self, t):
+        self.Solution_Time = t
+        self.Solution_P = np.ones( len(t)) * self.P_max
+        for i in range( len(t)):
+            if t[i]>=self.T0: continue
+            self.Solution_P[i] = self.K / (np.exp(self.A*(self.T0 - t[i]))-1)
+        return
+
+P0 = Population(2061.9005, 8400, 0.027)
+T0 = np.linspace( 1900, 2200, 301)
+T_sf  = np.linspace(-3000, 3000, 6001)
+P_sf = P0.Solve( T_sf)
+
+P1 = Population_vonFoerster()
+P1.Solve( np.linspace(-3000, 3000, 6001))
+P2 = Population_vonHoerner()
+P2.Solve( np.linspace(-3000, 3000, 6001))
+P3 = Population_vonFoerster_Full()
+P3.Solve( np.linspace(-3000, 3000, 6001))
+
+P4 = Population_Kapitsa()
+P4.Solve( T_sf)
+P5 = Population_Kapitsa( T0=2003, K=176e3, tau=44)
+P5.Solve( T_sf)
+
+Magic_Year = np.pi/2 - 10e3/176e3*44
+Magic_Year = 2003 - 44 * np.tan( Magic_Year)
+print( Magic_Year)
+
+Prepare_Russian_Font()
+fig = plt.figure( figsize=(15,10))
+plt.plot( T0, P0.UN_Medium.GetVector(T0), "-", lw=2, color="g", label="Средняя оценка ООН, 2010 г")
+plt.plot( T_sf, P_sf, "--", lw=1, color="g", label="Модель Акулий Плавник")
+#plt.plot( P1.Solution_Time, P1.Solution_P, "-", lw=1, color="b", label="Аспиранты фон-Фёрстера, 1960 г")
+#plt.plot( P2.Solution_Time, P2.Solution_P, "-", lw=1, color="r", label="Шутка фон-Хёрнера, 1975 г")
+#plt.plot( P3.Solution_Time, P3.Solution_P, "--", lw=1, color="b", label="Полное решение фон-Фёрстера")
+plt.plot( P4.Solution_Time, P4.Solution_P, "--", lw=1, color="r", label="Статья С.П.Капицы, 1996 г")
+plt.plot( P5.Solution_Time, P5.Solution_P, "-", lw=2, color="r", label="T0=2003, K=176e9, tau=44")
+plt.plot( [Magic_Year, Magic_Year], [1,100000], "-.", lw=3, color="r", label="Магическая дата: пятница, 25 ноября 2061 г")
+plt.plot( [1900, 2200], [10000,10000], "-.", lw=3, color="r", label="Великий демографический переход Капицы 10 млрд.")
+plt.errorbar( P0.Calibration_Year, P0.Calibration_Total, yerr=P0.Calibration_Delta, fmt='o', color="b", label="Население (статистика ООН)")
+plt.xlabel("Годы")
+plt.xlim( 1900, 2200)
+plt.ylabel("миллионов человек")
+plt.ylim( 0, 20000)
+plt.title( 'Подбор С, tau, To в формуле С.П.Капицы по данным 2016 года')
+plt.grid(True)
+plt.legend(loc=0)
+plt.savefig( ".\\Graphs\\figure_05_04.png")
+fig.show()
---
+++ Chapter 05/Test_Island_2.py
@@ -0,0 +1,105 @@
+from Population import *
+
+#
+# Описывает популяцию в открытой системе
+# Согласно системе уравнений Мальтуса
+# P0 - начальная популяция
+# Q0 - начальные запасы
+# O0 - начальное производство
+# F0 - начальные природные ресурсы
+# T2_P - время удвоения популяции
+# T2_O - время удвоения призводства
+# T2_F - время восстановления ресурсов
+# d_Q - процент потери запасов
+# j_O - технологический коэффициент урожайности
+# Популяция стремится к кажущемуся оптимуму Q
+#
+class Island_Population_2:
+    def __init__( self, P0, Q0, O0, F0, T2_P, T2_O, T2_F, d_Q, j_O):
+        self.P_Initial = P0
+        self.P = P0
+        self.Q_Initial = Q0
+        self.Q = Q0
+        self.O_Initial = O0
+        self.O = O0
+        self.F_Initial = F0
+        self.F = F0
+        self.B_P = np.log(2)/T2_P
+        self.B_O = np.log(2)/T2_O
+        self.B_F = np.log(2)/T2_F
+        self.D = Sigmoid( 150, 0.05, 1, d_Q)
+        self.J = j_O
+        return
+    def dP_dt( self, t):
+        tmp = max ( self.Q, 0.01)
+        tmp = self.B_P * (1 - self.P / tmp)
+        tmp *= self.P
+        return tmp
+    def dQ_dt( self, t):
+        tmp = -self.D.Compute(t) * self.Q # естественная убыль запасов
+        tmp -= self.P          # убыль запасов на потребление
+        tmp += self.O          # текущий урожай
+        return tmp
+    def dO_dt( self, t):
+        tmp = max ( self.F * self.J, 0.01)
+        tmp = self.B_O * (1 - self.P / tmp)
+        tmp *= self.O
+        return tmp
+    def dF_dt( self, t):
+        tmp = self.B_F * (1-self.F/self.F_Initial) * self.F     # естественное восстановление ресурса
+        tmp -= self.O                                           # убыль ресурса на пополнение запасов
+        return tmp
+    def _func( self, y, t):
+        self.P = max( [y[0], 0])
+        self.Q = max( [y[1], 0])
+        self.O = max( [y[2], 0])
+        self.F = max( [y[3], 0])
+        f0 = self.dP_dt( t)
+        f1 = self.dQ_dt( t)
+        f2 = self.dO_dt( t)
+        f3 = self.dF_dt( t)
+        return [f0, f1, f2, f3]
+    def Solve( self, t0):
+        y0 = [self.P, self.Q, self.O, self.F]
+        soln = odeint(self._func, y0, t0, h0=0.01, hmax = 0.025)
+        self.Solution_Time = t0
+        self.Solution_P = soln[:, 0].clip(0)
+        self.Solution_Q = soln[:, 1].clip(0)
+        self.Solution_O = soln[:, 2].clip(0)
+        self.Solution_F = soln[:, 3].clip(0)
+        self.P = self.P_Initial
+        self.Q = self.Q_Initial
+        self.O = self.O_Initial
+        self.F = self.F_Initial
+        return
+
+#
+# Solve numerically
+#
+T = np.linspace(0, 1000, 1001)
+t2_q = 10 # восстановление ресурсов
+productivity = 0.001
+P1 = Island_Population_2( 500, 500, 1000, 500000, 25, 5, t2_q, 0.3, productivity)
+P1.Solve( T)
+b1 = np.log(2) / 25
+Exponent1 = 500 * np.exp( b1*(T-0))
+
+for i in range( len(T)):
+    print( "{:4g} {:6.1f} {:6.1f} {:6.1f} {:8.1f}".format( T[i], P1.Solution_P[i], P1.Solution_Q[i], P1.Solution_O[i], P1.Solution_F[i]))
+
+Prepare_Russian_Font()
+fig = plt.figure( figsize=(15,10))
+plt.plot( P1.Solution_Time, P1.Solution_P, "-", lw=2, label="Население")
+plt.plot( P1.Solution_Time, P1.Solution_Q, "-", lw=2, label="Запасы")
+plt.plot( P1.Solution_Time, P1.Solution_O, "-", lw=1, label="Производство {:5.2f}% от F".format( productivity*100))
+plt.plot( P1.Solution_Time, P1.Solution_F/100, "-", lw=1, label="Природные ресурсы (х100)")
+plt.plot( T, Exponent1, "--", lw=1, label="Экспоненциальный рост Т2=25 лет")
+plt.xlabel("Годы")
+plt.xlim( 0, 500)
+plt.ylabel("единиц")
+plt.ylim( 0, 10000)
+plt.title( "Остров Мальтуса #2 - Первое технологическое достижение.")
+plt.grid(True)
+plt.legend(loc=0)
+plt.savefig( ".\\Graphs\\figure_05_06.png")
+fig.show()
