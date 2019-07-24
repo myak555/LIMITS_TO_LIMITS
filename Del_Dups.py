fba6052f259e7d6e87d51945c4e3cf863317aa1b
myak555 <33710266+myak555@users.noreply.github.com>
2019-07-24 17:54:55 +1000

    Refactoring Utilities

--- Del_Dups.py
+++ Del_Dups.py
@@ -3,25 +3,29 @@ from os.path import isfile, join, exists
 
 
 def GetNames(Source_Folder):
-    return [f for f in listdir(Source_Folder) if isfile(join(Source_Folder, f))]
+    return [f for f in listdir(Source_Folder)
+            if isfile(join(Source_Folder, f))]
 
 
 def DeleteInFolder(Name, Folder):
     dest_name = join(Folder, Name)
-    if not exists(dest_name): return
+    if not exists(dest_name):
+        return
     remove(dest_name)
     print("Deleted: {:s}".format(dest_name))
 
 
 def DeleteInAllChapters(Name):
-    if Name is None: return
-    for chapterNumber in range(1,100):
+    if Name is None:
+        return
+    for chapterNumber in range(1, 100):
         dest_name = "./Chapter {:02d}/".format(chapterNumber)
-        if not exists(dest_name): continue
+        if not exists(dest_name):
+            continue
         DeleteInFolder(Name, dest_name)
         DeleteInFolder(Name, dest_name + "/Data/")
 
 
-Names = GetNames( "./Global Data/")
+Names = GetNames("./Global Data/")
 for n in Names:
     DeleteInAllChapters(n)
--- Propagate.py
+++ Propagate.py
@@ -4,7 +4,7 @@ import shutil
 
 
 def GetSource():
-    if len( sys.argv) <= 2:
+    if len(sys.argv) <= 2:
         print("Usage:")
         print("Propagate <source_folder> <file_name> [start_folder]")
         return None, None, 3
@@ -13,15 +13,16 @@ def GetSource():
     Start_Folder = 3
     if len(sys.argv) >= 4:
         try:
-            Start_Folder = int( sys.argv[3])
-        except:
+            Start_Folder = int(sys.argv[3])
+        except Exception:
             print("[start_folder] must be a number")
             return None, None, 3
-    if not '.' in Source_File: Source_File += ".py" 
+    if '.' not in Source_File:
+        Source_File += ".py"
     try:
         n = int(Source_Folder)
         Full_Name = "./Chapter {:02d}/{:s}".format(n, Source_File)
-    except:
+    except ValueError:
         Full_Name = "./{:s}/{:s}".format(Source_Folder, Source_File)
     if not os.path.exists(Full_Name):
         print("File not found: {:s}".format(Full_Name))
@@ -31,22 +32,27 @@ def GetSource():
 
 
 def CopyToAllFolders(Source_Name, Destination_Name, Start_Folder):
-    if Source_Name is None: return
-    if Destination_Name is None: return
-    for chapterNumber in range(Start_Folder,100):
+    if Source_Name is None:
+        return
+    if Destination_Name is None:
+        return
+    for chapterNumber in range(Start_Folder, 100):
         dest_name = "./Chapter {:02d}/".format(chapterNumber)
-        if not os.path.exists(dest_name): continue
+        if not os.path.exists(dest_name):
+            continue
         dest_name += Destination_Name
-        if dest_name == Source_Name: continue 
+        if dest_name == Source_Name:
+            continue
         if not os.path.exists(dest_name):
             print(" * {:s} - not at destination".format(dest_name))
             continue
         try:
-            shutil.copy( Source_Name, dest_name)
+            shutil.copy(Source_Name, dest_name)
             print("   {:s}".format(dest_name))
-        except:
+        except FileNotFoundError:
             print(" * {:s} - failed to copy".format(dest_name))
+    return
 
 
 a, b, n = GetSource()
-CopyToAllFolders( a, b, n)
+CopyToAllFolders(a, b, n)
